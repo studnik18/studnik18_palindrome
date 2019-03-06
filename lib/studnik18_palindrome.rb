c0faf5c3f695304f4f1fcc38ee77a8c9a0a24eb6
Maciej Studniarz <maciejstudniarz@gmail.com>
2019-03-06 11:05:52 +0100

    Finish working and refactored palindrome method

--- lib/studnik18_palindrome.rb
+++ lib/studnik18_palindrome.rb
@@ -1,6 +1,17 @@
 require "studnik18_palindrome/version"
 
-module Studnik18Palindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  def letters
+    self.chars.select { |c| c.match(/[a-z]/i) }.join
+  end
+
+  private
+
+    def processed_content
+      self.letters.downcase
+    end
 end
--- test/studnik18_palindrome_test.rb
+++ test/studnik18_palindrome_test.rb
@@ -6,6 +6,23 @@ class Studnik18PalindromeTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
+
+  def test_non_palindrome
+    assert !"apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "Racecar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam".palindrome?
+  end
+
 end
